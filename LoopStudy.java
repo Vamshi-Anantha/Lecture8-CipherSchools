fc9c1aaeec2006808509876276d153262b8a166d
Vamshi-Anantha <124914610+Vamshi-Anantha@users.noreply.github.com>
2023-07-18 13:40:55 +0530

    Add files via upload

---
+++ LoopStudy.java
@@ -0,0 +1,33 @@
+public class LoopStudy {
+    public static void main(String args[]) {
+        int i;
+
+        // FOR LOOP
+        for (i = 1; i <= 100; i++) {
+            System.out.println(i);
+        }
+        // loop will break if i=101
+        System.out.println("i is : " + i);
+        System.out.println("*************************************");
+        for (i = 1; i <= 100; i++) {
+            System.out.println(i + 1);
+        }
+        System.out.println("i is : " + i);
+
+        // WHILE LOOP
+        int n = 5;
+        while (n > 0) {
+            System.out.println("Hello");
+            n--;
+        }
+        System.out.println("After while loop, n is : " + n);
+
+        // DO WHILE LOOP - It runs atleast once
+        n = 5;
+        do {
+            System.out.println(n);
+            n--;
+        } while (n > 0);
+        System.out.println("After DO-While loop n is : " + n);
+    }
+}
